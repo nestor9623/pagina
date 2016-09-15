f6d1b46a82151e24e5b4c5acfdd61e8885c355db
Nestor Franco <411pc20@10.1.4.47>
2016-09-14 20:42:18 -0500

    Mejora para el commit de la nueva clase
    se agregan los campos de nombre y apellido junto con una clave
    
    tipo:mejora

--- appPagina/src/modelo/Persona.java
+++ appPagina/src/modelo/Persona.java
@@ -13,27 +13,35 @@ public class Persona {
     
     private String id ;
     private String nombre;
+    private String clave;
+    private String apellido;
 
-    public Persona(String id, String nombre) {
+    public Persona(String id, String nombre, String clave, String apellido) {
         this.id = id;
         this.nombre = nombre;
+        this.clave = clave;
+        this.apellido = apellido;
     }
 
     public String getId() {
         return id;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
+    public String getClave() {
+        return clave;
     }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    
+    
+    
     
     
     
